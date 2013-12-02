cdefa49b75977e605e2f8c86873ce085a786f22c
LoLOffline <lolofflinee@gmail.com>
2013-12-02 22:07:19 +0100

    Initial release

---
+++ src/LoLOffline/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LoLOffline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rbFirewall = new System.Windows.Forms.RadioButton();
+            this.rbConfig = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lChatServer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go offline";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Go online";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rbFirewall
+            // 
+            this.rbFirewall.AutoSize = true;
+            this.rbFirewall.Checked = true;
+            this.rbFirewall.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFirewall.Location = new System.Drawing.Point(12, 12);
+            this.rbFirewall.Name = "rbFirewall";
+            this.rbFirewall.Size = new System.Drawing.Size(102, 17);
+            this.rbFirewall.TabIndex = 2;
+            this.rbFirewall.TabStop = true;
+            this.rbFirewall.Text = "Firewall mode\r\n";
+            this.rbFirewall.UseVisualStyleBackColor = true;
+            // 
+            // rbConfig
+            // 
+            this.rbConfig.AutoSize = true;
+            this.rbConfig.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbConfig.Location = new System.Drawing.Point(12, 67);
+            this.rbConfig.Name = "rbConfig";
+            this.rbConfig.Size = new System.Drawing.Size(95, 17);
+            this.rbConfig.TabIndex = 3;
+            this.rbConfig.Text = "Config mode";
+            this.rbConfig.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "No game restart needed\r\nWindows Firewall must be enabled";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Game must be restarted to take effect";
+            // 
+            // lChatServer
+            // 
+            this.lChatServer.AutoSize = true;
+            this.lChatServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lChatServer.Location = new System.Drawing.Point(12, 117);
+            this.lChatServer.Name = "lChatServer";
+            this.lChatServer.Size = new System.Drawing.Size(67, 13);
+            this.lChatServer.TabIndex = 7;
+            this.lChatServer.Text = "Chat server: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 184);
+            this.Controls.Add(this.lChatServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbConfig);
+            this.Controls.Add(this.rbFirewall);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoL Offline";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton rbFirewall;
+        private System.Windows.Forms.RadioButton rbConfig;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lChatServer;
+    }
+}
+
---
+++ src/LoLOffline/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+
+namespace LoLOffline
+{
+    public partial class Form1 : Form
+    {
+        private String configFile = "";
+        private String configFileBackup = "";
+        private String xmppHost = "";
+        private String xmppIp = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void error(String message, Exception ex = null)
+        {
+            if (ex != null) message += Environment.NewLine + Environment.NewLine + ex.Message;
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(0);
+        }
+
+        private String getRADSDir()
+        {
+            try
+            {
+                RegistryKey regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Riot Games\RADS\");
+                return regKey.GetValue("LocalRootFolder").ToString();
+            }
+            catch (Exception ex)
+            {
+                error("Unable to get LoL install Directory", ex);
+            }
+            return "";
+        }
+
+        private String getConfigFile()
+        {
+            try
+            {
+                String radsDir = getRADSDir();
+                String airDir = this.getConfigSetting(radsDir + @"\system\launcher.cfg", "airConfigProject");
+                String[] files = Directory.GetFiles(radsDir + @"\projects\" + airDir + @"\releases", @"lol.properties", SearchOption.AllDirectories);
+                Array.Sort(files, StringComparer.InvariantCulture);
+                return files[files.Length - 1];
+            }
+            catch (Exception ex)
+            {
+                error("Error while trying to read config file", ex);
+            }
+            return "";
+        }
+
+        private String getConfigSetting(String path, String setting)
+        {
+            try
+            {
+                String content = File.ReadAllText(path);
+                Regex myRegex = new Regex(setting + @"=(.*)");
+                Match m = myRegex.Match(content);
+                if (m.Success)
+                {
+                    return m.Groups[1].Value.ToString().Trim();
+                }
+            }
+            catch (Exception ex)
+            {
+                error("Unable to read setting '" + setting + "' from file: " + path, ex);
+            }
+            return "";
+        }
+
+        private void setConfigSetting(String path, String setting, String value)
+        {
+            try
+            {
+                String content = File.ReadAllText(path);
+                content = Regex.Replace(content, setting + "=.*" + Environment.NewLine, setting + "=" + value + Environment.NewLine);
+                File.WriteAllText(path, content);
+            }
+            catch (Exception ex)
+            {
+                error("Unable to set setting '" + setting + "' in file: " + path, ex);
+            }
+        }
+
+        private void createHostBackup()
+        {
+            try
+            {
+                String host = getConfigSetting(this.configFile, "xmpp_server_url");
+                if (host.Contains("riotgames"))
+                {
+                    File.WriteAllText(configFileBackup, host);
+                }
+            }
+            catch (Exception ex)
+            {
+                error("Unable to create host backup file: " + configFileBackup, ex);
+            }
+        }
+
+        private String getHost()
+        {
+            return File.ReadAllText(this.configFileBackup).Trim();
+        }
+
+        private String getHostIp()
+        {
+            try
+            {
+                IPHostEntry hostEntry;
+                hostEntry = Dns.GetHostEntry(this.xmppHost);
+                if (hostEntry.AddressList.Length > 0)
+                {
+                    return hostEntry.AddressList[0].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                error("Error when trying to resolve " + this.xmppHost, ex);
+            }
+            return "";
+        }
+
+        private void executeCommand(String cmd)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + cmd;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private void goOffLineFirewall()
+        {
+            try
+            {
+                executeCommand("netsh advfirewall firewall delete rule name=\"LoLOffline\"");
+                executeCommand("netsh advfirewall firewall add rule dir=in name=\"LoLOffline\" action=block enable=yes localip=any remoteip=" + this.xmppIp);
+                executeCommand("netsh advfirewall firewall add rule dir=out name=\"LoLOffline\" action=block enable=yes localip=any remoteip=" + this.xmppIp);
+            }
+            catch (Exception ex)
+            {
+                error("Unable to set firewall rules", ex);
+            }
+        }
+
+        private void goOnLineFirewall()
+        {
+            try
+            {
+                executeCommand("netsh advfirewall firewall delete rule name=\"LoLOffline\"");
+            }
+            catch (Exception ex)
+            {
+                error("Unable to set firewall rules", ex);
+            }
+        }
+
+        private void goOffLineConfig()
+        {
+            setConfigSetting(this.configFile, "xmpp_server_url", "localhost");
+        }
+
+        private void goOnLineConfig()
+        {
+            setConfigSetting(this.configFile, "xmpp_server_url", this.xmppHost);
+        }
+
+        private void reset()
+        {
+            try { goOnLineFirewall(); } catch (Exception ex) { };
+            try { goOnLineConfig(); } catch (Exception ex) { };
+        }
+
+        private void loadSettings()
+        {
+            rbFirewall.Checked = Properties.Settings.Default.FirewallOrConfig;
+            rbConfig.Checked = !Properties.Settings.Default.FirewallOrConfig;
+        }
+
+        private void saveSettings()
+        {
+            Properties.Settings.Default.FirewallOrConfig = rbFirewall.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadSettings();
+
+            this.configFile = getConfigFile();
+            this.configFileBackup = this.configFile + ".loloffline";
+            
+            createHostBackup();
+
+            this.xmppHost = this.getHost();
+            this.xmppIp = this.getHostIp();
+
+            lChatServer.Text = "Chat Server: " + this.xmppHost + " [" + this.xmppIp + "]";
+
+            try { goOnLineFirewall(); } catch (Exception ex) { };
+            goOnLineConfig();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveSettings();
+            reset();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reset();
+            if (rbFirewall.Checked) goOffLineFirewall();
+            if (rbConfig.Checked) goOffLineConfig();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
